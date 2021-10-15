c2f945056de47aeab5530d4bc35d42eb6330fbc1
hudtakim <hudtakim@gmail.com>
2021-10-15 08:45:33 +0700

    revisi route

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ export default class App extends Component {
         <NavbarComponent />
         <JumbotronComponent />
         <BrowserRouter basename={'/loan-app'}>
-          <Route path="/" exact component={HomeContainer} />
-          <Route path="/create" exact component={CreateUserContainer} />
-          <Route path="/edit/:id" exact component={EditUserContainer} />
-          <Route path="/detail/:id" exact component={DetailUserContainer} />
+          <Route path={`${process.env.PUBLIC_URL}/`} exact component={HomeContainer} />
+          <Route path={`${process.env.PUBLIC_URL}/create`} exact component={CreateUserContainer} />
+          <Route path={`${process.env.PUBLIC_URL}/edit/:id`} exact component={EditUserContainer} />
+          <Route path={`${process.env.PUBLIC_URL}/detail/:id`} exact component={DetailUserContainer} />
         </BrowserRouter>
       </div>
     )
